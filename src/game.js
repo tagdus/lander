ae03a3c54c4e9161bdb5f3f24e2cf3601b0c2f49
tagdus <tagdus@yahoo.com>
2021-02-11 15:03:27 +0100

    Bozza calcolo forze

--- src/game.js
+++ src/game.js
@@ -4,20 +4,20 @@ var con = can.getContext("2d");
 can.width = can.height * (can.clientWidth / can.clientHeight);
 
 var ar = [];
-var an = 0;
-var vl = 0;
+var va = 0;
+var vm = 0;
 
 for (var i = 0; i < 50; i++) {
-  an = Math.random() * 2 * Math.PI;
-  vl = Math.random() * 3 - 1;
-  vl = vl * 0.1;
+  va = Math.random() * 2 * Math.PI;
+  vm = Math.random() * 3 - 1;
+  vm = vm * 0.1;
   ar.push({
     x_: Math.random() * can.width,
     y_: Math.random() * can.height,
-    dx: vl * Math.cos(an),
-    dy: vl * Math.sin(an),
-    vl: vl,
-    an: an,
+    dx: vm * Math.cos(va),
+    dy: vm * Math.sin(va),
+    vm: vm,
+    va: va,
     cl: "#" + Math.floor(Math.random() * 16777215).toString(16)
   });
 }
@@ -26,21 +26,30 @@ function draw() {
   con.clearRect(0, 0, can.width, can.height);
 
   for (var i = 0; i < ar.length; i++) {
-    ar[i].an = ar[i].an + (Math.random() - 0.5) * 0.1 * Math.PI;
-    //ar[i].vl = ar[i].vl * (1 + Math.random() - 0.5) * 0.1;
-    ar[i].dx = ar[i].vl * Math.cos(ar[i].an);
-    ar[i].dy = ar[i].vl * Math.sin(ar[i].an);
+    ar[i].va = ar[i].va + (Math.random() - 0.5) * 0.1 * Math.PI;
+    //ar[i].vm = ar[i].vm * (1 + Math.random() - 0.5) * 0.1;
+    ar[i].dx = ar[i].vm * Math.cos(ar[i].va);
+    ar[i].dy = ar[i].vm * Math.sin(ar[i].va);
 
     ar[i].x_ = (ar[i].x_ + ar[i].dx) % can.width;
     ar[i].y_ = (ar[i].y_ + ar[i].dy) % can.height;
 
     //calcola la forza di tutti su ciascuno
-    //for(var i=0;i<ar.length;i++)
-    //  for(var j=0,var ar[i].fx=0;j<ar.length;j++)
-    //    if(i!=j) {
-    //      ar[i].fx =+ Math.cos(ang(i,j)) *  k / dst(i,j);
-    //      ar[i].fy =+ Math.sin(ang(i,j)) *  k / dst(i,j);k / dist(i,j);
+    //for(var i = 0, var fx = 0, var fy = 0; i < ar.length; i++){
+    //  for(var j = 0; j < ar.length; j++)
+    //    if(i != j) {
+    //      fx =+ Math.cos(ang(i, j)) *  k / dst(i, j);
+    //      fy =+ Math.sin(ang(i, j)) *  k / dst(i, j);
     //    }
+    //  ar[i].fm = modul(fx,fy);
+    //  ar[i].fa = angle(fx,fy);
+    //  ar[i].vm = ??;
+    //  ar[i].va = ??;
+    //  ar[i].dm = ??;
+    //  ar[i].da = ??;
+    //  ar[i].dx = ar[i].dm * Math.cos(ar[i].da);
+    //  ar[i].dy = ar[i].dm * Math.sin(ar[i].da);
+    //}
 
 
     con.beginPath();
